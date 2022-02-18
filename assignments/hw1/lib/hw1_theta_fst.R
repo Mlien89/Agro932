7da1625bcbe45276781e7472c390c24fa295db57
Mason.Lien <mason.lien@syngenta.com>
2022-02-17 20:30:18 -0600

    updated r script

--- assignments/hw1/lib/hw1_theta_fst.R
+++ assignments/hw1/lib/hw1_theta_fst.R
@@ -1,14 +1,11 @@
 ########################
 # Agro - 932 Homework 1#
-# Mason Lien           #
+# Author: Mason Lien   #
 ########################
 
-
-setwd("C:/Users/u942451/OneDrive - University of Nebraska-Lincoln/school/PhD/PhD_github/Agro932/assignments/hw1/agro932_hw1")
-
-#load Geno.txt file
-
-geno <- read.table("data/arabidopsis/mutation_01/geno.txt", header=FALSE)
+#load Geno.txt file 
+#file is stored in data folder in mutation_01
+geno <- read.table(file.choose()) #choose the geno.txt file created from the hw1_arab.sh where the mutation rate is 0.01 
 names(geno) <- c("chr", "pos", "ref", "alt", "l1", "l2", "l3", "l4", "l5", "l6", "l7", "l8", "l9", "l10", "l11", "l12", "l13", "l14", "l15", "l16", "l17", "l18", "l19", "l20")
 head(geno)
 
@@ -30,11 +27,29 @@ for(i in 5:24){
 
 geno$p <- apply(geno[, 25:64], 1, function(x) {sum(as.numeric(as.character(x)))})
 geno$p <- geno$p/40
-geno$p1 <- apply(geno[, 25:54], 1, function(x) {sum(as.numeric(as.character(x)))})
-geno$p1 <- geno$p1/30
-geno$p2 <- apply(geno[, 55:64], 1, function(x) {sum(as.numeric(as.character(x)))})
-geno$p2 <- geno$p2/10
+geno$p1 <- apply(geno[, 25:47], 1, function(x) {sum(as.numeric(as.character(x)))})
+geno$p1 <- geno$p1/24
+geno$p2 <- apply(geno[, 48:64], 1, function(x) {sum(as.numeric(as.character(x)))})
+geno$p2 <- geno$p2/16
 
+
+#theta function
+pi <- function(n,p){
+  return(n/(n-1)*(1-p^2-(1-p)^2))
+}
+
+#compute theta for population
+for (i in seq_len(nrow(geno))){
+  geno$theta_p1 <- pi(n=20, p=geno$p1)
+  geno$theta_p2 <- pi(n=20, p=geno$p2)
+}
+
+
+pdf(file = "graphs/theta_wmutation01.PDF",   # The directory you want to save the file in
+    width = 4, # The width of the plot in inches
+    height = 4) # The height of the plot in inches
+plot(geno$pos, geno$theta, xlab="Physical position", ylab="theta value", main="")
+dev.off()
 
 #calculate Fst
 geno$fst <- with(geno, ((p1-p)^2 + (p2-p)^2)/(2*p*(1-p)))
@@ -53,8 +68,7 @@ dev.off()
 
 
 ###run same for mutation rate of 10%
-
-geno <- read.table("data/arabidopsis/mutation_10/geno.txt", header=FALSE)
+geno <- read.table(file.choose()) #choose the geno.txt file created from the hw1_arab.sh where the mutation rate is 0.10
 names(geno) <- c("chr", "pos", "ref", "alt", "l1", "l2", "l3", "l4", "l5", "l6", "l7", "l8", "l9", "l10", "l11", "l12", "l13", "l14", "l15", "l16", "l17", "l18", "l19", "l20")
 head(geno)
 
@@ -71,16 +85,28 @@ for(i in 5:24){
   names(geno)[ncol(geno)] <- paste0(nm, sep="_a2")
 }
 
+
 #calculate Fst value for each site and visualize the results
 #compute p1, p2, p with a 60/40 
 
 geno$p <- apply(geno[, 25:64], 1, function(x) {sum(as.numeric(as.character(x)))})
 geno$p <- geno$p/40
-geno$p1 <- apply(geno[, 25:54], 1, function(x) {sum(as.numeric(as.character(x)))})
-geno$p1 <- geno$p1/30
-geno$p2 <- apply(geno[, 55:64], 1, function(x) {sum(as.numeric(as.character(x)))})
-geno$p2 <- geno$p2/10
+geno$p1 <- apply(geno[, 25:47], 1, function(x) {sum(as.numeric(as.character(x)))})
+geno$p1 <- geno$p1/24
+geno$p2 <- apply(geno[, 48:64], 1, function(x) {sum(as.numeric(as.character(x)))})
+geno$p2 <- geno$p2/16
 
+#compute theta
+
+pi <- function(n,p){
+  return(n/(n-1)*(1-p^2-(1-p)^2))
+}
+
+#compute theta for population
+for (i in seq_len(nrow(geno))){
+  geno$theta_p1 <- pi(n=20, p=geno$p1)
+  geno$theta_p2 <- pi(n=20, p=geno$p2)
+}
 
 #calculate Fst
 geno$fst <- with(geno, ((p1-p)^2 + (p2-p)^2)/(2*p*(1-p)))
@@ -96,7 +122,6 @@ pdf(file = "graphs/Fst_wmutation10.PDF",   # The directory you want to save the 
     height = 4) # The height of the plot in inches
 plot(fst$pos, fst$fst, xlab="Physical position", ylab="Fst value", main="")
 dev.off()
-
 
 
 
